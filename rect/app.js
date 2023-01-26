c4e1427b79d3be5f33e961a982502d058a284517
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-01-26 20:13:35 +0630

    add new

--- rect/app.js
+++ rect/app.js
@@ -1,68 +1,31 @@
-const scrollArea = document.querySelector("#areaOne");
-const sections = document.querySelectorAll(".section-one");
+const main = document.querySelector("main")
+// const circle = document.querySelector(".circle");
 
-const options = {
-  root: scrollArea,
-  rootMargin: "10px",
-  threshold: 1,
-};
+const react = document.querySelector('.section-two');
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(getIntersectionMeta(entry));
-  });
-}, options);
+const observer = new IntersectionObserver(entries => {
 
-sections.forEach((el) => observer.observe(el));
+  entries.forEach(entry => {
+    const target = entry.target;
+    const interSectionRatio = entry.intersectionRatio;
+    const rootBounds = entry.rootBounds;
+    const boundingClientRect = entry.boundingClientRect;
+    const intersectionRect = entry.intersectionRect;
+    const isIntersecting = entry.isIntersecting;
 
-/** Area two start */
-const scrollAreaTwo = document.querySelector("#areaTwo");
-const sectionsTwo = document.querySelectorAll(".section-two");
 
-const optionsTwo = {
-  root: scrollAreaTwo,
-  rootMargin: "100px 0px 100px 0px",
-  threshold: 1,
-};
+    const str =`
+      ${target.textContent}:
+      isIntersecting: ${isIntersecting},
+      boundingClientRect: x:${boundingClientRect.x}, y:${boundingClientRect.y}, top:${boundingClientRect.top}, bottom: ${boundingClientRect.bottom}
+      intersectionRect: ${intersectionRect},
+      
+      rootBounds: ${rootBounds},
+      ratio: ${interSectionRatio},
+      --------------------------------------
+      `
 
-const observerTwo = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(getIntersectionMeta(entry));
-  });
-}, optionsTwo);
+      console.log(str);
+  })
 
-sectionsTwo.forEach((el) => observerTwo.observe(el));
-
-function getIntersectionMeta(entry) {
-  const boundingClientRect = entry.boundingClientRect;
-  const isIntersecting = entry.isIntersecting;
-  const intersectionRatio = entry.intersectionRatio;
-  const intersectionRect = entry.intersectionRect;
-  const rootBounds = entry.rootBounds;
-  const target = entry.target;
-  const time = entry.time;
-
-  return {
-    name: target.textContent,
-    boundingClientRect,
-    isIntersecting,
-    intersectionRatio,
-    intersectionRect,
-    rootBounds,
-    target,
-    time,
-  };
-}
-
-function buildThresholdList() {
-  let thresholds = [];
-  let numSteps = 20;
-
-  for (let i = 1.0; i <= numSteps; i++) {
-    let ratio = i / numSteps;
-    thresholds.push(ratio);
-  }
-
-  thresholds.push(0);
-  return thresholds;
-}
+})
