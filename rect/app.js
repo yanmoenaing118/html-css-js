25665f94e9b5868936101b4749b4f12b577f5d93
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-01-26 20:26:49 +0630

    i love it

--- rect/app.js
+++ rect/app.js
@@ -2,6 +2,11 @@ const main = document.querySelector("main")
 // const circle = document.querySelector(".circle");
 
 const react = document.querySelector('.section-two');
+const meta = document.querySelector('.meta');
+
+console.log(react);
+
+
 
 const observer = new IntersectionObserver(entries => {
 
@@ -15,17 +20,31 @@ const observer = new IntersectionObserver(entries => {
 
 
     const str =`
-      ${target.textContent}:
-      isIntersecting: ${isIntersecting},
-      boundingClientRect: x:${boundingClientRect.x}, y:${boundingClientRect.y}, top:${boundingClientRect.top}, bottom: ${boundingClientRect.bottom}
-      intersectionRect: ${intersectionRect},
-      
-      rootBounds: ${rootBounds},
-      ratio: ${interSectionRatio},
-      --------------------------------------
+    <pre>
+isIntersecting: ${isIntersecting},
+
+boundingClientRect: x:${boundingClientRect.x}, y:${boundingClientRect.y}, top:${boundingClientRect.top}, bottom: ${boundingClientRect.bottom}
+
+intersectionRect: x:${intersectionRect.x}, y:${intersectionRect.y}, top:${intersectionRect.top}, bottom: ${intersectionRect.bottom},
+
+rootBounds:${JSON.stringify(rootBounds)},
+
+ratio: ${interSectionRatio},
+      </pre>
       `
 
-      console.log(str);
+      // console.log(str);
+      meta.innerHTML = str;
   })
 
+}, {
+  root: main,
+  threshold: buildThreshold(100)
 })
+
+
+observer.observe(react);
+
+function buildThreshold(steps) {
+  return new Array(steps).fill(0).map((el,idx) => (idx + 1)/ steps)
+}
