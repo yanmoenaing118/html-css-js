066ad9ee7ec35acc8d50196659f9bbceee260053
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-01-28 21:32:04 +0630

    intersectioin area

--- scroll/app.js
+++ scroll/app.js
@@ -1,38 +1,75 @@
-const sectionTwo = document.querySelector(".section-two");
+const ratio = document.querySelector(".ratio");
+const scrollDirection = document.querySelector(".scrollDirection");
+const targetPosition = document.querySelector(".targetPositon");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const target = entry.target;
-      const isIntersecting = entry.isIntersecting;
-      const img = target.querySelector(".image");
-      const ratio = entry.intersectionRatio;
-      console.log(ratio);
-      if (isIntersecting) {
-        console.log(isIntersecting);
-        const tY = ratio;
-        console.log(tY);
-        img.style.transform = `translateY(${200 * ratio}px)`;
-      } else {
-        // img.style.transform = `translateY(0px)`
-      }
-    });
-  },
-  {
-    threshold: buildThresholdList(20),
-  }
-);
+const WINDOW_HEIGHT = window.innerHeight;
+const WINDOW_WIDTH = window.innerWidth;
 
-observer.observe(sectionTwo);
+console.log(`
+WINDOW_WIDTH: ${WINDOW_WIDTH}
+WINDOW_HEIGHGT: ${ WINDOW_HEIGHT}
+`);
 
-function buildThresholdList(numSteps) {
-  let thresholds = [];
+const target = document.querySelector(".section_3");
+const {
+  x: targetX,
+  y: targetY,
+  top: targetTop,
+  right: targetRight,
+  bottom: targetBotom,
+  left: targetLeft,
+} = target.getBoundingClientRect();
 
-  for (let i = 1.0; i <= numSteps; i++) {
-    let ratio = i / numSteps;
-    thresholds.push(ratio);
-  }
+console.log(targetPosition);
 
-  thresholds.push(0);
-  return thresholds;
+console.log(target);
+
+window.addEventListener("scroll", function (e) {
+  const {
+    x: targetX,
+    y: targetY,
+    top: targetTop,
+    right: targetRight,
+    bottom: targetBotom,
+    left: targetLeft,
+    height: targetHeight
+  } = target.getBoundingClientRect();
+
+  const isIntersecting = targetBotom > 0;
+  
+  const intersectingArea = WINDOW_HEIGHT - Math.abs(targetTop);
+
+
+  // console.log(Math.abs(targetY - WINDOW_HEIGHT))
+  console.log(intersectingArea)
+
+  updateMeta({
+    targetX,
+    targetY,
+    targetTop,
+    targetRight,
+    targetBotom,
+    targetLeft,
+    isIntersecting,
+    targetHeight
+  });
+});
+
+function updateMeta(meta) {
+  targetPosition.innerHTML = `
+  <pre>
+  ---------------------
+  TARGET position
+  x: ${meta.targetX},
+  y: ${meta.targetY},
+  top: ${meta.targetTop},
+  right: ${meta.targetRight},
+  bottom: ${meta.targetBotom},
+  left: ${meta.targetLeft}
+  height: ${meta.targetHeight}
+  ---------------------
+  isIntersecting: ${meta.isIntersecting}
+  </pre>
+  
+    `;
 }
